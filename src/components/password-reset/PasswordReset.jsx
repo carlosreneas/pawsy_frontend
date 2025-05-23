973125b8fb019b1fa3f352023fc9d49456e7b595
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2025-05-23 12:00:56 -0500

    Added background,  new password doesnt appear on url, wider card

--- src/components/password-reset/PasswordReset.jsx
+++ src/components/password-reset/PasswordReset.jsx
@@ -1,31 +1,57 @@
 import React from "react";
 import Box from "@mui/material/Box";
-import MuiCard from '@mui/material/Card';
-import AppTheme from '../shared-theme/AppTheme';
+import MuiCard from "@mui/material/Card";
+import AppTheme from "../shared-theme/AppTheme";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 
 const Card = styled(MuiCard)(({ theme }) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  alignSelf: 'center',
-  width: '100%',
+  display: "flex",
+  flexDirection: "column",
+  alignSelf: "center",
+  width: "100%",
   padding: theme.spacing(4),
   gap: theme.spacing(2),
-  margin: 'auto',
-  height: '100%',
-  [theme.breakpoints.up('sm')]: {
-    maxWidth: '450px',
+  margin: "auto",
+  height: "100%",
+  [theme.breakpoints.up("sm")]: {
+    maxWidth: "700px",
   },
   boxShadow:
-    'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
-  ...theme.applyStyles('dark', {
+    "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
+  ...theme.applyStyles("dark", {
     boxShadow:
-      'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
+      "hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px",
   }),
+}));
+
+const PageContainer = styled(Stack)(({ theme }) => ({
+  // height: 'calc((1 - var(--template-frame-height, 0)) * 100dvh)',
+  overflow: "auto",
+  minHeight: "100%",
+  padding: theme.spacing(2),
+  [theme.breakpoints.up("sm")]: {
+    padding: theme.spacing(4),
+  },
+  "&::before": {
+    content: '""',
+    display: "block",
+    position: "fixed",
+    zIndex: -1,
+    inset: 0,
+    backgroundImage:
+      "radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))",
+    backgroundRepeat: "no-repeat",
+    ...theme.applyStyles("dark", {
+      backgroundImage:
+        "radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))",
+    }),
+  },
 }));
 
 export default function PasswordReset() {
@@ -35,15 +61,15 @@ export default function PasswordReset() {
   const [confirmErrorMessage, setConfirmErrorMessage] = React.useState("");
 
   const handleSubmit = (event) => {
+    event.preventDefault();
     if (newError || confirmError) {
-      event.preventDefault();
       return;
     }
     const data = new FormData(event.currentTarget);
     console.log({
       new_password: data.get("new_password"),
-      confirm_password: data.get("confirm_password"),
     });
+    alert(data);
   };
 
   const validateInputs = () => {
@@ -57,6 +83,7 @@ export default function PasswordReset() {
       setNewErrorMessage("Password must be at least 6 characters long.");
       isValid = false;
     } else {
+      setNewErrorMessage("");
       setNewError(false);
     }
 
@@ -65,6 +92,7 @@ export default function PasswordReset() {
       setConfirmErrorMessage("Passwords do not match.");
       isValid = false;
     } else {
+      setConfirmErrorMessage("");
       setConfirmError(false);
     }
 
@@ -73,60 +101,69 @@ export default function PasswordReset() {
 
   return (
     <AppTheme>
-      <Card variant="outlined">
-        <Box
-          component="form"
-          onSubmit={handleSubmit}
-          noValidate
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            width: "100%",
-            gap: 2,
-          }}
-        >
-          <FormControl>
-            <FormLabel htmlFor="new_password">Enter new password</FormLabel>
-            <TextField
-              error={newError}
-              helperText={newErrorMessage}
-              id="new_password"
-              type="password"
-              name="new_password"
-              placeholder="New password"
-              autoFocus
-              required
-              fullWidth
-              variant="outlined"
-              color={newError ? "error" : "primary"}
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="confirm_password">Confirm Password</FormLabel>
-            <TextField
-              error={confirmError}
-              helperText={confirmErrorMessage}
-              id="confirm_password"
-              type="password"
-              name="confirm_password"
-              placeholder="Confirm password"
-              autoFocus
-              required
-              fullWidth
-              variant="outlined"
-              color={confirmError ? "error" : "primary"}
-            />
-          </FormControl>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            onClick={validateInputs}
+      <PageContainer>
+        <Card variant="outlined">
+            <Typography
+            component="h1"
+            variant="h4"
+            sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
           >
-            Set new password
-          </Button>
-        </Box>
-      </Card>
+            Recover Password
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              width: "100%",
+              gap: 2,
+            }}
+          >
+            <FormControl>
+              <FormLabel htmlFor="new_password">Enter new password</FormLabel>
+              <TextField
+                error={newError}
+                helperText={newErrorMessage}
+                id="new_password"
+                type="password"
+                name="new_password"
+                placeholder="New password"
+                autoFocus
+                required
+                fullWidth
+                variant="outlined"
+                color={newError ? "error" : "primary"}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="confirm_password">Confirm Password</FormLabel>
+              <TextField
+                error={confirmError}
+                helperText={confirmErrorMessage}
+                id="confirm_password"
+                type="password"
+                name="confirm_password"
+                placeholder="Confirm password"
+                autoFocus
+                required
+                fullWidth
+                variant="outlined"
+                color={confirmError ? "error" : "primary"}
+              />
+            </FormControl>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              onClick={validateInputs}
+            >
+              Set new password
+            </Button>
+          </Box>
+        </Card>
+      </PageContainer>
     </AppTheme>
   );
 }
